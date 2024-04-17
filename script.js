683acb6b19b1f09272f7ca2c43b7d488f6961138
Vinicius <pollyanatrader@gmail.com>
2024-04-17 15:39:26 -0300

    ok

---
+++ script.js
@@ -0,0 +1,17 @@
+var Pessoa = /** @class */ (function () {
+    function Pessoa() {
+    }
+    return Pessoa;
+}());
+var pessoa = new Pessoa();
+var entrada = require('prompt-sync')();
+var n = entrada('Insira seu nome: ');
+console.log('Bem vindo ' + n);
+pessoa.nome = n;
+var i = entrada('Insira sua idade: ');
+console.log('Voce tem ' + i + ' Anos');
+pessoa.idade = i;
+var p = entrada('Insira sua profissão: ');
+console.log('Sua profissão é ' + p);
+pessoa.profissao = p;
+console.log(pessoa);
